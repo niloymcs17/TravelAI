d3b0fc1e86bfae7be4cae573b176cbedac2271bd
niloymcs17 <niloy.mcs17.du@gmail.com>
2024-08-02 18:59:24 +0530

    Updated Stepper form

--- app/create-trip/search-place.tsx
+++ app/create-trip/search-place.tsx
@@ -13,7 +13,8 @@ export default function SearchPlace() {
     const [destinationAddress, setDestinationAddress] = useState<any>();
     const [selectedTraveler, setSelectedTraveler] = useState<any>();
     const [formState, setFormState] = useState<any>(1); // Start with the initial state
-    const [travelDates, setTravelDates] = useState({ startDate: null, endDate: null });
+    const [startDate, setStartDate] = useState();
+    const [endDate, setEndDate] = useState();
 
     useEffect(() => {
         navigation.setOptions({
@@ -22,80 +23,118 @@ export default function SearchPlace() {
         });
     }, []);
 
-    useEffect(() => {
-        if (destinationAddress) {
-            setFormState(2);
-            console.log(destinationAddress);
+    const handleNext = () => {
+        if (formState === 1 && !destinationAddress) {
+            alert('Please select a destination.');
+            return;
+        } else if (formState === 2 && !selectedTraveler) {
+            alert('Please select a traveler.');
+            return;
+        } else if (formState === 3 && !(startDate && endDate)) {
+            alert('Please select a Dates .');
+            return;
         }
-    }, [destinationAddress]);
+        console.log(startDate +"-"+ endDate)
+        setFormState(formState + 1);
+    };
 
-    useEffect(() => {
-        if (selectedTraveler) {
-            setFormState(3);
-            console.log(selectedTraveler);
+    const handlePrevious = () => {
+        if (formState > 1) {
+            setFormState(formState - 1);
         }
-    }, [selectedTraveler]);
+    };
 
-    const onNextDates = (startDate: any, endDate: any) => {
-        setTravelDates({ startDate, endDate });
-        console.log('Start Date:', startDate);
-        console.log('End Date:', endDate);
+    const onNextDates = (date:any, type:any) => {
+        type === 'END_DATE' ? setEndDate(date) : setStartDate(date);
+        console.log('Date:', date);
+        console.log('type:', type);
         // Handle the dates here, e.g., navigate to another screen or update formState
     };
 
     return (
         <View style={styles.container}>
-            <View style={{flex:1}}>
-                <Text style={[STYLE_GLOBAL.headerText, { fontFamily: FONT.BOLD }]}>Where are you travelling to?</Text>
-                <GooglePlacesAutocomplete
-                    styles={{
-                        textInputContainer: {
-                            borderWidth: 2,
-                            borderRadius: 5,
-                            margin: 5
-                        },
-                    }}
-                    placeholder='Search'
-                    onPress={(data, details = null) => {
-                        setDestinationAddress({
-                            name: data.description,
-                            coordinates: details?.geometry?.location,
-                            url: details?.url,
-                        });
-                    }}
-                    query={{
-                        key: "AIzaSyAJQsu6pd9FQovj0Hgd7JCp19WL0mq0BUY",
-                        language: 'en',
-                    }}
-                />
+            {formState === 1 && (
+                <View style={{ flex: 1 }}>
+                    <Text style={[STYLE_GLOBAL.headerText, { fontFamily: FONT.BOLD }]}>Where are you travelling to?</Text>
+                    <GooglePlacesAutocomplete
+                        styles={{
+                            textInputContainer: {
+                                borderWidth: 2,
+                                borderRadius: 5,
+                                margin: 5
+                            },
+                        }}
+                        placeholder='Search'
+                        onPress={(data, details = null) => {
+                            setDestinationAddress({
+                                name: data.description,
+                                coordinates: details?.geometry?.location,
+                                url: details?.url,
+                            });
+                        }}
+                        query={{
+                            key: "AIzaSyAJQsu6pd9FQovj0Hgd7JCp19WL0mq0BUY",
+                            language: 'en',
+                        }}
+                    />
+                </View>
+            )}
+            {formState === 2 && (
+                <View style={{ flex: 1 }}>
+                    <Text style={[STYLE_GLOBAL.headerText]}>Who is traveling</Text>
+                    <FlatList
+                        data={SelectTravelerList}
+                        renderItem={({ item }) => (
+                            <Pressable onPress={() => setSelectedTraveler(item.title)}>
+                                <OptionCard item={item} selectedTraveler={selectedTraveler} />
+                            </Pressable>
+                        )}
+                        keyExtractor={item => item.id.toString()}
+                    />
+                </View>
+            )}
+            {formState === 3 && (
+                <SelectDate onNextDates={onNextDates} />
+            )}
+            <View style={styles.navigationButtons}>
+                {formState > 1 && (
+                    <Pressable style={styles.button} onPress={handlePrevious}>
+                        <Text style={styles.buttonText}>Back</Text>
+                    </Pressable>
+                )}
+                {formState < 4 && (
+                    <Pressable style={styles.button} onPress={handleNext}>
+                        <Text style={styles.buttonText}>Next</Text>
+                    </Pressable>
+                )}
             </View>
-            <View style={{ flex: 1 }}>
-                <Text style={[STYLE_GLOBAL.headerText]}>Who is traveling</Text>
-                <FlatList
-                    data={SelectTravelerList}
-                    renderItem={({ item }) => (
-                        <Pressable onPress={() => setSelectedTraveler(item.title)}>
-                            <OptionCard item={item} selectedTraveler={selectedTraveler} />
-                        </Pressable>
-                    )}
-                />
-            </View>
-            <SelectDate onNextDates={onNextDates} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    mapSearch: {
-        borderWidth: 2,
-        borderColor: "black",
-        borderRadius: 8,
-    },
     container: {
         flex: 1,
         width: "100%",
         padding: 10,
         backgroundColor: "white",
         alignContent: "flex-start",
+    },
+    navigationButtons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+    button: {
+        backgroundColor: 'black',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontFamily: FONT.MEDIUM,
+        textAlign: 'center',
     },
 });
--- components/select-date.tsx
+++ components/select-date.tsx
@@ -5,27 +5,12 @@ import CalendarPicker from 'react-native-calendar-picker';
 import { Colors } from '@/constants/Colors';
 
 export default function SelectDate({ onNextDates }:any) {
-  const [startDate, setStartDate] = useState();
-  const [endDate, setEndDate] = useState();
-  const [snackbarVisible, setSnackbarVisible] = useState(false);
-
-  const onDateChange = (date:any, type:any) => {
-    type === 'END_DATE' ? setEndDate(date) : setStartDate(date);
-  };
-
-  const onNext = () => {
-    if (startDate && endDate) {
-      onNextDates(startDate, endDate);
-    } else {
-      console.error("Date not selected")
-    }
-  };
 
   return (
     <View>
       <Text style={STYLE_GLOBAL.headerText}>Select travel dates</Text>
       <CalendarPicker
-        onDateChange={onDateChange}
+        onDateChange={onNextDates}
         allowRangeSelection={true}
         minDate={new Date()}
         maxRangeDuration={7}
@@ -36,9 +21,6 @@ export default function SelectDate({ onNextDates }:any) {
           color: Colors.white,
         }}
       />
-      <Pressable onPress={onNext} style={STYLE_GLOBAL.button}>
-        <Text style={STYLE_GLOBAL.btext}>Next</Text>
-      </Pressable>
     </View>
   );
 }
